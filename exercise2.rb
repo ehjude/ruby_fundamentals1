115abc1beec93994b07a641dbf21e0803e8d0009
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-10-21 13:26:56 -0400

    Complete the 2nd exercise

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+# calculate tip for meal
+tip = 55*0.15
+puts "The tip on $55 is #{tip}"
+
+# add an integer and string
+puts "This is a string that has a number: " + 55.to_s
+
+# multiple some numbers
+puts "45628 x 7839 equals #{45628 * 7839}"
+
+# boolean fun
+puts (true && false) || (false && true) || !(false && false)
